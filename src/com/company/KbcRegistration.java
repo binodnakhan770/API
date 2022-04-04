f21aa835e1a889f9c46db152520d77287919a4c4
BinodNakhan <binodnakhan95@gmail.com>
2022-04-04 17:30:34 +0545

    intitial commit

---
+++ src/com/company/KbcRegistration.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import jdk.jfr.ContentType;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KbcRegistration {
+
+    private static final String KBC_REGISTRATION_API_URI = "http://devapi.thedoko.com/api/kbc-registration";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String payload = """
+                data ={
+                     "fullName": "Binod Nakhan",
+                     "email": "binod@gmail.com",
+                     "phone": "9860876238",
+                     "countryCode": "+977",
+                     "quantity": 100
+                 }
+               
+                """;
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(payload))
+                .header("accept","application/json")
+                .uri(URI.create(KBC_REGISTRATION_API_URI))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    private static String KBC_VALUE_API_URI = "https://devapi.thedoko.com/api";
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept","application/json")
+                .uri(URI.create(KBC_VALUE_API_URI))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+    }
+}
